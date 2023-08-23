132170713f60a1cd536446089979023f2afbabb7
Oscar <oscar_apaza07@hotmail.com>
2023-08-23 02:44:09 -0300

    routers y controllers

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express= require('express');
 const app= express();
 const path= require('path');
+const mainRouter = require("./src/routes/mainRouter")
+const detalleRouter = require("./src/routes/detalleRouter")
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+
+app.use( '/', mainRouter);
+app.use('/detalle', detalleRouter);
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 
 const PORT= 3000;
 
 app.listen(PORT,()=>console.log('Corriendo en el puerto: '+PORT));
-app.get('/register',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/register.html'))});
-app.post('/register',(req,res)=>{
-    res.redirect('/');
-})
-app.get('/home',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, './index.html'))});
-app.get('/detalle',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, './views/detalle.html'))});
+
+
+
+
+//app.get('/register',(req,res)=>{
+//    res.sendFile(path.resolve(__dirname, './views/register.html'))});
+//app.post('/register',(req,res)=>{
+//    res.redirect('/');
+//})
+//app.get('/home',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname, './index.html'))});
+//app.get('/detalle',(req,res)=>{
+//        res.sendFile(path.resolve(__dirname, './views/detalle.html'))});
---
+++ src/controller/detalleController.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const detalleController = {
+    detalle: (req, res)=>{
+        res.render('detalle');
+    }
+}
+
+module.exports = detalleController;
