046fd1d296cb6b335d827499aa5d68c384628394
nicolee567 <nicole.mendivil.cabrera@gmail.com>
2023-08-22 21:59:11 -0300

    nicole:agregue src en la direcciones index,detalle,carro,register

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ const PORT= 3030;
 app.set('view','ejs');
 app.set('views','./src/views');
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'))});
+    res.sendFile(path.resolve(__dirname, './src/views/index.html'))});
 
 app.get('/detalle',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, './views/detalle.html'))});
+        res.sendFile(path.resolve(__dirname, './src/views/detalle.html'))});
 
 app.get('/productos',(req,res)=>{
-            res.sendFile(path.resolve(__dirname, './views/carro.html'))});
+            res.sendFile(path.resolve(__dirname, './src/views/carro.html'))});
         
 
 app.get('/register',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/register.html'))});
+    res.sendFile(path.resolve(__dirname, './src/views/register.html'))});
 
 app.post('/register',(req,res)=>{
     res.redirect('/')
 })
 app.get('/login',(req,res)=>{
-    res.sendFile((path.resolve(__dirname, './views/login.html')))});
+    res.sendFile((path.resolve(__dirname, './src/views/login.html')))});
  
 
 app.post('/login',(req,res)=>{
@@ -33,7 +33,7 @@ app.post('/login',(req,res)=>{
     })
 
 app.get('/carro',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/carro.html'))
+    res.sendFile(path.resolve(__dirname, './src/views/carro.html'))
 });
     
 app.post('/carro',(req,res)=>{
